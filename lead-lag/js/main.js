99937f71f752da98c6efea4eb3f0cf111fd8575c
Brandon Sprague <brandon00sprague@gmail.com>
2014-02-01 07:53:32 -0500

    Fixing leonards requests

--- lead-lag/js/main.js
+++ lead-lag/js/main.js
@@ -12,19 +12,34 @@ $(function(){
 function updateGraph(){
   var omega = parseInt($('.omega').val()) || 50;
   var tmin = parseFloat($('.tmin').val()) || -0.2;
-  var tmax = parseInt($('.tmax').val()) || 0.3;
+  var tmax = parseFloat($('.tmax').val()) || 0.3;
   var deg = parseInt($('.deg').val()) || 90;
   var base = [];
   var shifted = [];
-  for(var i = tmin*100; i < tmax*100; i += 1){
-    base.push([i,Math.sin(omega*i)]);
-    shifted.push([i,Math.sin(omega*i+deg*Math.PI/180)]);
+  var base_label = "cos("+omega+"x)";
+  var shifted_label = "cos("+omega+"x+"+deg+")";
+  for(var i = tmin*1000; i < tmax*1000; i += 1){
+    base.push([i,Math.cos(omega*i/1000)]);
+    shifted.push([i,Math.cos(omega*i/1000+deg*Math.PI/180)]);
   }
-  var plot = $.plot('.plot',[base,shifted], {
+  var plot = $.plot('.plot',[
+      {data: base, label: base_label},
+      {data: shifted, label: shifted_label}
+      ], {
     series: {
       lines: {
         show: true
-      }
-    }
+      },
+    },
+    xaxes: [{
+      axisLabel: 'Time (ms)',
+      axisLabelPadding: 10,
+      axisLabelUseCanvas: true
+    }],
+    yaxes: [{
+      axisLabel: 'f(t)',
+      axisLabelPadding: 10,
+      axisLabelUseCanvas: true
+    }]
   });
 }
