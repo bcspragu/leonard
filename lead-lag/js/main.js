6c061ff8027a3e2bf6de2f22ad680707e8398f23
Brandon Sprague <brandon00sprague@gmail.com>
2014-02-02 13:05:08 -0500

    More fixes, dynamic y-axis

--- lead-lag/js/main.js
+++ lead-lag/js/main.js
@@ -10,12 +10,20 @@ $(function(){
 
 
 function updateGraph(){
+  var amp_min = -1;
+  var amp_max = 1;
   var omega = nanDefault(parseFloat($('.omega').val()),50);
   var tmin = nanDefault(parseFloat($('.tmin').val()),-0.2);
   var tmax = nanDefault(parseFloat($('.tmax').val()),0.3);
   var deg = nanDefault(parseFloat($('.deg').val()),90);
   var base = [];
   var shifted = [];
+  var vertical = [];
+  var non_zero_axis = tmin < 0 && tmax > 0;
+  if(non_zero_axis){
+    vertical.push([0,-1]);
+    vertical.push([0,1]);
+  }
   var coefficient = omega == 1 ? "" : omega;
   var base_label = "cos("+coefficient+"t)";
   var shifted_label = "";
@@ -27,6 +35,15 @@ function updateGraph(){
     shifted_label = "cos("+coefficient+"t)";
   }
   var factor = omega/10;
+  var grid = {labelMargin: 10};
+  if(non_zero_axis){
+    grid.borderWidth = {bottom: 2, left: 2, top:2, right: 2},
+    grid.borderColor = {};
+  }else{
+    grid.borderWidth = {bottom: 2, left: 3, top:2, right: 2},
+    grid.borderColor = {left: 'black'};
+  }
+
   for(var i = tmin*1000; i < tmax*1000; i += 1/factor){
     base.push([i,Math.cos(omega*i/1000)]);
     shifted.push([i,Math.cos(omega*i/1000+deg*Math.PI/180)]);
@@ -34,6 +51,7 @@ function updateGraph(){
   var baseline = [[tmin*1000,0],[tmax*1000,0]];
   var plot = $.plot('.plot',[
       {data: baseline, color: 'black', shadowSize: 0},
+      {data: vertical, color: 'black', shadowSize: 0, lineWidth: 3},
       {data: base, label: base_label},
       {data: shifted, label: shifted_label}
       ], {
@@ -52,15 +70,13 @@ function updateGraph(){
       axisLabel: 'f(t)',
       axisLabelPadding: 10,
       axisLabelUseCanvas: true,
-      font: {size: 15, color: 'black'}
+      font: {size: 15, color: 'black'},
+      min: amp_min,
+      max: amp_max
     }],
-    grid: {
-      borderWidth: {bottom: 2, left: 3, top:2, right: 2},
-      borderColor: {left: 'black'},
-      labelMargin: 10
-    }
-  });
-  var ll = leadLag(fixDeg(deg),omega)
+    grid: grid
+      });
+  var ll = leadLag(fixDeg(deg),omega);
   $('.note').text('The function '+shifted_label+'  is '+ll.ll+' '+base_label+' by '+ll.delay+'  ms');
 }
 
