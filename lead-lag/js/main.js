f5ebe976df45c73ef482d54688698590475f92b7
Brandon Sprague <brandon00sprague@gmail.com>
2014-02-02 11:36:22 -0500

    More fixes per leonard

--- lead-lag/js/main.js
+++ lead-lag/js/main.js
@@ -10,19 +10,32 @@ $(function(){
 
 
 function updateGraph(){
-  var omega = parseInt($('.omega').val()) || 50;
-  var tmin = parseFloat($('.tmin').val()) || -0.2;
-  var tmax = parseFloat($('.tmax').val()) || 0.3;
-  var deg = parseInt($('.deg').val()) || 90;
+  var omega = nanDefault(parseFloat($('.omega').val()),50);
+  var tmin = nanDefault(parseFloat($('.tmin').val()),-0.2);
+  var tmax = nanDefault(parseFloat($('.tmax').val()),0.3);
+  var deg = nanDefault(parseFloat($('.deg').val()),90);
+  deg = fixDeg(deg);
+  $('.deg').val(deg);
   var base = [];
   var shifted = [];
-  var base_label = "cos("+omega+"x)";
-  var shifted_label = "cos("+omega+"x+"+deg+")";
-  for(var i = tmin*1000; i < tmax*1000; i += 1){
+  var coefficient = omega == 1 ? "" : omega;
+  var base_label = "cos("+coefficient+"x)";
+  var shifted_label = "";
+  if(deg > 0){
+    shifted_label = "cos("+coefficient+"x+"+deg+"°)";
+  }else if (deg < 0){
+    shifted_label = "cos("+coefficient+"x"+deg+"°)";
+  }else{
+    shifted_label = "cos("+coefficient+"x)";
+  }
+  var factor = omega/10;
+  for(var i = tmin*1000; i < tmax*1000; i += 1/factor){
     base.push([i,Math.cos(omega*i/1000)]);
     shifted.push([i,Math.cos(omega*i/1000+deg*Math.PI/180)]);
   }
+  var baseline = [[tmin*1000,0],[tmax*1000,0]];
   var plot = $.plot('.plot',[
+      {data: baseline, color: 'black', shadowSize: 0},
       {data: base, label: base_label},
       {data: shifted, label: shifted_label}
       ], {
@@ -32,14 +45,48 @@ function updateGraph(){
       },
     },
     xaxes: [{
-      axisLabel: 'Time (ms)',
+      axisLabel: 'time, t (ms)',
       axisLabelPadding: 10,
-      axisLabelUseCanvas: true
+      axisLabelUseCanvas: true,
+      font: {size: 15, color: 'black'}
     }],
     yaxes: [{
       axisLabel: 'f(t)',
       axisLabelPadding: 10,
-      axisLabelUseCanvas: true
-    }]
+      axisLabelUseCanvas: true,
+      font: {size: 15, color: 'black'}
+    }],
+    grid: {
+      borderWidth: {bottom: 2, left: 3, top:2, right: 2},
+      borderColor: {left: 'black'},
+      labelMargin: 10
+    }
   });
+  var ll = leadLag(deg,omega)
+  $('.note').text('The function '+shifted_label+'  is '+ll.ll+' '+base_label+' by '+ll.delay+'  ms');
+}
+
+function nanDefault(value,def){
+  return isNaN(value) ? def : value
+}
+
+function fixDeg(deg){
+  while(deg < 0){
+    deg += 360;
+  }
+  while(deg >= 360){
+    deg -= 360;
+  }
+  return deg;
+}
+
+function leadLag(deg,omega){
+  var ll = {ll: 'leading', delay: 0};
+  ll.delay = (1000*(deg*Math.PI)/(180*omega));
+  if(deg > 180){
+    ll.ll = "lagging";
+    ll.delay = 2*Math.PI/omega*1000 - ll.delay;
+  }
+  ll.delay = ll.delay.toFixed(2);
+  return ll;
 }
