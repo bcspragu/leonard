fc5f464cfa52eb201ca08155c919beef214591a2
Brandon Sprague <brandon00sprague@gmail.com>
2014-02-04 22:01:41 -0500

    Finishing (hopefully) the new graph

--- vis-prod/js/main.js
+++ vis-prod/js/main.js
@@ -1,6 +1,10 @@
 $(function(){
-  var graph = $('.plot');
-  graph.height(graph.width()/2);
+  var graph1 = $('.plot1');
+  graph1.height(graph1.width()/3);
+  var graph2 = $('.plot2');
+  graph2.height(graph2.width()/3);
+  var graph3 = $('.plot3');
+  graph3.height(graph3.width()/3);
   updateGraph();
 
   $('.update-graph').click(function(){
@@ -10,60 +14,47 @@ $(function(){
 
 
 function updateGraph(){
-  var amp = nanDefault(parseFloat($('.amp').val()),1);
-  var graph = nanDefault(parseFloat($('.graph').val()),5);
-  var omega = nanDefault(parseFloat($('.omega').val()),50);
-  var deg = nanDefault(parseFloat($('.deg').val()),0);
-  var graph_min = -Math.abs(graph);
-  var graph_max = Math.abs(graph);
-  var base = [];
-  var vertical = [];
-  var tmin = -0.2;
-  var tmax = 0.3;
-  var non_zero_axis = tmin < 0 && tmax > 0;
+  var amp1 = nanDefault(parseFloat($('.amp1').val()),1);
+  var omega1 = nanDefault(parseFloat($('.omega1').val()),50);
+  var deg1 = nanDefault(parseFloat($('.deg1').val()),0);
 
-  if(non_zero_axis){
-    vertical.push([0,graph_min]);
-    vertical.push([0,graph_max]);
-  }
+  var amp2 = nanDefault(parseFloat($('.amp2').val()),1);
+  var omega2 = nanDefault(parseFloat($('.omega2').val()),50);
+  var deg2 = nanDefault(parseFloat($('.deg2').val()),0);
 
-  var amp_str = "";
-  if(amp == -1){
-    amp_str = "-"
-  }else if(amp != 1){
-    amp_str = amp;
-  }
-  var coefficient = omega == 1 ? "" : omega;
-  var base_label = amp_str+"cos("+coefficient+"t";
+  var base1 = [];
+  var base2 = [];
+  var res = [];
+  var tmax = 0.5;
 
-  if(deg > 0){
-    base_label += '+'+deg+'°)';
-  }else if(deg < 0){
-    base_label += deg+'°)';
-  }else{
-    base_label += ')';
-  }
+  var coefficient1 = omega1 == 1 ? "" : omega1;
+  var coefficient2 = omega2 == 1 ? "" : omega2;
+  var base_label1 = ampString(amp1)+"cos("+coefficient1+"t"+degString(deg1);
+  var base_label2 = ampString(amp2)+"cos("+coefficient2+"t"+degString(deg2);
 
-  var factor = omega/10;
+  var factor1 = omega1/10;
+  var factor2 = omega2/10;
   var grid = {labelMargin: 10};
 
-  if(non_zero_axis){
-    grid.borderWidth = {bottom: 2, left: 2, top:2, right: 2},
-    grid.borderColor = {};
-  }else{
-    grid.borderWidth = {bottom: 2, left: 3, top:2, right: 2},
-    grid.borderColor = {left: 'black'};
+  grid.borderWidth = {bottom: 2, left: 3, top:2, right: 2},
+  grid.borderColor = {left: 'black'};
+
+  for(var i = 0; i < tmax*1000; i += 1/factor1){
+    base1.push([i,amp1*Math.cos(omega1*i/1000+deg1*Math.PI/180)]);
   }
 
-  for(var i = tmin*1000; i < tmax*1000; i += 1/factor){
-    base.push([i,amp*Math.cos(omega*i/1000+deg*Math.PI/180)]);
+  for(var i = 0; i < tmax*1000; i += 1/factor2){
+    base2.push([i,amp2*Math.cos(omega2*i/1000+deg2*Math.PI/180)]);
   }
 
-  var baseline = [[tmin*1000,0],[tmax*1000,0]];
-  var plot = $.plot('.plot',[
+  for(var i = 0; i < tmax*1000; i += 1/factor2){
+    res.push([i,(amp1*Math.cos(omega1*i/1000+deg1*Math.PI/180))*(amp2*Math.cos(omega2*i/1000+deg2*Math.PI/180))]);
+  }
+
+  var baseline = [[0,0],[tmax*1000,0]];
+  var plot1 = $.plot('.plot1',[
       {data: baseline, color: 'black', shadowSize: 0},
-      {data: vertical, color: 'black', shadowSize: 0, lineWidth: 3},
-      {data: base, label: base_label},
+      {data: base1, label: base_label1},
       ], {
     series: {
       lines: {
@@ -77,16 +68,61 @@ function updateGraph(){
       font: {size: 15, color: 'black'}
     }],
     yaxes: [{
-      axisLabel: 'f(t)',
+      axisLabel: 'v(t)',
       axisLabelPadding: 10,
       axisLabelUseCanvas: true,
       font: {size: 15, color: 'black'},
-      min: graph_min,
-      max: graph_max
     }],
     grid: grid
       });
-  $('.note').text('The function '+base_label+' has a period of T = '+(1000*2*Math.PI/omega).toFixed(2)+' ms.');
+
+  var plot2 = $.plot('.plot2',[
+      {data: baseline, color: 'black', shadowSize: 0},
+      {data: base2, label: base_label2},
+      ], {
+    series: {
+      lines: {
+        show: true
+      },
+    },
+    xaxes: [{
+      axisLabel: 'time, t (ms)',
+      axisLabelPadding: 10,
+      axisLabelUseCanvas: true,
+      font: {size: 15, color: 'black'}
+    }],
+    yaxes: [{
+      axisLabel: 'i(t)',
+      axisLabelPadding: 10,
+      axisLabelUseCanvas: true,
+      font: {size: 15, color: 'black'},
+    }],
+    grid: grid
+      });
+
+  var plot3 = $.plot('.plot3',[
+      {data: baseline, color: 'black', shadowSize: 0},
+      {data: res, label: base_label1+' * '+base_label2},
+      ], {
+    series: {
+      lines: {
+        show: true
+      },
+    },
+    xaxes: [{
+      axisLabel: 'time, t (ms)',
+      axisLabelPadding: 10,
+      axisLabelUseCanvas: true,
+      font: {size: 15, color: 'black'}
+    }],
+    yaxes: [{
+      axisLabel: 'p(t)',
+      axisLabelPadding: 10,
+      axisLabelUseCanvas: true,
+      font: {size: 15, color: 'black'},
+    }],
+    grid: grid
+      });
 }
 
 function nanDefault(value,def){
@@ -101,4 +137,24 @@ function fixDeg(deg){
     deg -= 360;
   }
   return deg;
+}
+
+function ampString(amp){
+  var amp_str = "";
+  if(amp == -1){
+    amp_str = "-"
+  }else if(amp != 1){
+    amp_str = amp;
+  }
+  return amp_str;
+}
+
+function degString(deg){
+  var deg_str = ")";
+  if(deg > 0){
+    deg_str = '+'+deg+'°)';
+  }else if(deg < 0){
+    deg_str = deg+'°)';
+  }
+  return deg_str;
 }
