c344a314f4ffef0581f38bfcab874e9157b0e62d
Brandon Sprague <brandon00sprague@gmail.com>
2014-03-22 16:50:20 -0400

    Fixes and highlight max

--- reson/js/main.js
+++ reson/js/main.js
@@ -3,6 +3,11 @@ $(function(){
   graph.height(graph.width()/2);
   updateGraph();
 
+  $('.scale').click(function(){
+    $(this).toggleClass('success');
+    $('.gmax').parents('.columns').toggleClass('invisible');
+  });
+
   $('.update-graph').click(function(){
     updateGraph();
   });
@@ -12,29 +17,29 @@ $(function(){
 function updateGraph(){
   var v0 = nanDefault(parseFloat($('.volt').val()),1);
   var res = nanDefault(parseFloat($('.res').val()),50);
-  var ind = nanDefault(parseFloat($('.ind').val()),50);
-  var cap = nanDefault(parseFloat($('.cap').val()),50);
+  var ind = nanDefault(parseFloat($('.ind').val()),50)/1000;
+  var cap = nanDefault(parseFloat($('.cap').val()),50)/1000000;
+  var gmax = nanDefault(parseFloat($('.gmax').val()),0.05);
+  var max_amp = (v0*1.0/res);
+  var res_freq = (1/Math.sqrt(ind*cap));
+  //
+  // Only scale if they've turned on autoscale
+  var scale = !$('.scale').hasClass('success');
   var base = [];
 
 
-  var grid = {labelMargin: 10};
-
   for(var i = 0; i < 100000; i += 1){
-    base.push([i/10,(v0)/(Math.sqrt(Math.pow(res,2)+Math.pow((i/10)*(ind/1000)-1/((i/10)*cap/1000000),2)))]);
+    base.push([i/10,(v0)/(Math.sqrt(Math.pow(res,2)+Math.pow((i/10)*ind-1/((i/10)*cap),2)))]);
   }
 
-  var baseline = [[0,0],[10000,0]];
-  var plot = $.plot('.plot',[
-      {data: baseline, color: 'black', shadowSize: 0},
-      {data: base},
-      ], {
+  var options = {
     series: {
       lines: {
         show: true
       },
     },
     xaxes: [{
-      axisLabel: 'omega, (rad/s)',
+      axisLabel: 'ω, (rad/s)',
       axisLabelPadding: 10,
       axisLabelUseCanvas: true,
       font: {size: 15, color: 'black'}
@@ -43,10 +48,23 @@ function updateGraph(){
       axisLabel: 'I(t)',
       axisLabelPadding: 10,
       axisLabelUseCanvas: true,
-      font: {size: 15, color: 'black'}
+      font: {size: 15, color: 'black'},
+      min: 0
     }],
-    grid: grid
-      });
+    grid: {labelMargin: 10}
+      };
+
+  if(scale){
+    options.yaxes[0].max = gmax;
+  }
+
+  var max_res = [[res_freq,0],[res_freq,max_amp]];
+
+  var plot = $.plot('.plot',[
+      {data: max_res, color: 'black', shadowSize: 0},
+      {data: base},
+      ], options);
+  $('.note').text('Maximum amplitude of current is '+max_amp.toFixed(3)+' A at ω = '+res_freq.toFixed(2)+' rad/s.');
 }
 
 function nanDefault(value,def){
